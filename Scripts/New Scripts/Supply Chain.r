8dbfd8d654ebebebf3448a233e690f264ffaca37
Shida Nyimbili <Shida.Nyimbili@righttocare-zambia.org>
2022-09-30 21:56:23 +0200

    ns

--- Scripts/New Scripts/Supply Chain.r
+++ Scripts/New Scripts/Supply Chain.r
@@ -2,7 +2,7 @@
 source("scripts/r prep2.r")
 
 #'*Stock availability*
-stk <- read_xlsx("Data/RTC/Supply Chain/determine_TLD.xlsx")
+stk <- read_xlsx("Data/RTC/Supply Chain/determine_TLD 29.09.2022.xlsx")
 stk  <- stk  %>%
   mutate(month_chr = str_sub(period,
                              start=1,
@@ -17,6 +17,13 @@ stk  <- stk  %>%
          mnthyr = my(monyr))
 
 sum(stk$month_chr!=stk$month) # expecting 0 if vars same
+
+stk
+
+###Rename the cell names of provinces
+stk$Province_Determine <- gsub("Luapula _% Availability","Luapula", stk$Province_Determine)
+stk$Province_Determine <- gsub("Muchinga_% Availability","Muchinga", stk$Province_Determine)
+stk$Province_Determine <- gsub("Northern_% Availability","Northern", stk$Province_Determine)
 
 stk
 names(stk)
@@ -34,11 +41,19 @@ stk2 <- stk1 %>%
          TLD.Avail=5,
          mth=6)
 stk2
+names(stk2)
 
 ####plot Determine
 
 dtmin <- stk2 %>%
   select(1,2,3,6)
+
+TLD <- stk2 %>%
+  select(1,2,3,5,6)
+
+TLD
+
+
 
 names(dtmin)
 
@@ -47,7 +62,7 @@ names(dtmin)
 target <- as.Date("2021-10-01")
 
 dtmin <- dtmin %>% 
-  gather(key = subpop , value = rate, c(dtmine.avail,))
+  gather(key = subpop , value = rate, c(dtmine.avail))
 
 dtmin_plt <- ggplot(dtmin, aes(x = mth, y = rate, group = subpop, colour = subpop)) +
   geom_point(alpha=.6, size=1) + 
@@ -57,16 +72,22 @@ dtmin_plt <- ggplot(dtmin, aes(x = mth, y = rate, group = subpop, colour = subpo
                      breaks = c(.2, .4, .6, .8, 1)) +
   scale_x_date(date_labels="%b %y",date_breaks="2 months") +
   facet_wrap(~prov) + faceted +
-  labs(x="", y="", caption="Data Source: HMIS", title="Determine Stock Availability Trends by Province Oct 2021 to Sept 2022") +
+  labs(x="", y="", caption="Data Source: HMIS", title="") +
   scale_color_manual(name ="",
                      values = usaid_palette,
-                     labels = c("Stock availability")) + 
+                     labels = c("Determine Stock availability")) + 
   geom_hline(yintercept = .95 ,color=usaid_red ,
              lty="longdash",size=.7, alpha=.6) +
-  geom_text(aes(target, .95, label = "95% : Target Determine Stock Availability", vjust = -0.7, hjust=0.05),color=usaid_red, size=3)  + basey
+  geom_text(aes(target, .95, 
+                label = "95% : Target Determine Stock Availability", 
+                vjust = -0.7, hjust=0.05),color=usaid_red, size=3) + 
+  basey
 
 dtmin_plt
-ggsave("Viz/RTC/Supply Chain/Determine Draft.png",
+
+plot_layout(dtmin_plt, guides = "collect")
+
+ggsave("Viz/RTC/Supply Chain/Det Draft.png",
        device="png",
        type="cairo",
        height = 6.5,
