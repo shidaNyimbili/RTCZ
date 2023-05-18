e967d7bdfbc9ceb1a0fe7b5bcc6a50ec4f6e7cc2
Shida Nyimbili <nyimbilishida@gmail.com>
2023-05-18 17:01:02 +0200

    ns

--- Scripts/r prep2.r
+++ Scripts/r prep2.r
@@ -31,7 +31,7 @@ zam_packages <- c("tidyverse", "here", "gifski", "gt","gtsummary","gghighlight",
                   "leaflet", "ggsflabel", "sf", "RSelenium", "netstat", "GGally", "calendR", "areaplot", "hexbin",
                   "webshot", "oceanis", "SPlit", "gsubfn", "proto","fpp", "mapview", "shinydashboard", "packrat",
                   "gt", "bigD","formattable", "reactablefmtr", "scales", "caret","predict3d", "ggpmisc", "cowplot", 
-                  "googleway", "ggplot2", "ggrepel", "ggspatial", "libwgeom", "sp", "rnaturalearth", "rnaturalearthdata", "pacman", "vroom", "rgdal","gridExtra")
+                  "googleway", "ggplot2", "ggrepel", "giscoR", "ggspatial", "sp", "rnaturalearth", "rnaturalearthdata", "pacman", "vroom", "rgdal","gridExtra")
 
 
 
