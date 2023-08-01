dee192d5bdc64f55fafd6cf75af962c0d757f2bf
Shida Nyimbili <nyimbilishida@gmail.com>
2023-08-01 23:06:36 +0200

    ns

---
+++ Scripts/New Scripts/Animation graphs.r
@@ -0,0 +1,20 @@
+# Get data:
+library(gapminder)
+
+# Charge libraries:
+library(ggplot2)
+library(gganimate)
+
+# Make a ggplot, but add frame=year: one image per year
+ggplot(gapminder, aes(gdpPercap, lifeExp, size = pop, color = continent)) +
+  geom_point() +
+  scale_x_log10() +
+  theme_bw() +
+  # gganimate specific bits:
+  labs(title = 'Year: {frame_time}', x = 'GDP per capita', y = 'life expectancy') +
+  transition_time(year) +
+  ease_aes('linear')
+
+# Save at gif:
+anim_save("271-ggplot2-animated-gif-chart-with-gganimate1.gif"
+          
