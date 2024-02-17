2c10e2e00d02a8e353b6f99e068f64758d4c9658
Shida Nyimbili <nyimbilishida@gmail.com>
2024-02-17 22:44:01 +0200

    ns

--- Scripts/dingani.r
+++ Scripts/dingani.r
@@ -70,7 +70,7 @@ p1 + p2 + plot_layout(guides = "collect")
 
 ggsave("viz/Dingani/smoothplots.png",
        device="png",
-       type="cairo",
+       type="Cairo",
        height = 6.0,
        width = 13)
 
@@ -137,7 +137,100 @@ count_unique_obs <- subset_ofg %>%
 count_unique_obs
 write.xlsx(count_unique_obs, "Data/Dingani/count_unique_obs.xlsx", rowNames = FALSE)
 
+## Question 4:
+#1. Using the `mDat.csv` dataset again, take the natural logarithm of the
+#"b_weight" column and store it as a new variable (column) in the dataframe.
+
+mDat
+mDat$log_b_weight <- log(mDat$b_weight)
+
+mDat$log_b_weight
+
+# 2. Calculate the mean, standard deviation, and sample size for each taxonomic order
+stats_mean.StDV <- mDat %>%
+  group_by(order) %>%
+  summarise(mean_log_b_weight = mean(log_b_weight, na.rm = TRUE),
+            sd_log_b_weight = sd(log_b_weight, na.rm = TRUE),
+            n = n())
+stats_mean.StDV
+write.xlsx(stats_mean.StDV, "Data/Dingani/summary_stats.xlsx", rowNames = FALSE)
+
+# 3. Calculate the standard error for the log(b_weight) variable for each taxonomic order
+stats_mean.StDV.error <- stats_mean.StDV %>%
+  mutate(SE_log_b_weight = sd_log_b_weight / sqrt(n))
+
+stats_mean.StDV.error
+
+write.xlsx(stats_mean.StDV.error, "Data/Dingani/summary_stats_error.xlsx", rowNames = FALSE)
+
+# 4. Calculate the mean value for the `litters_yr` variable
+#This value is stored in "mean_litters_yr" column
+mean_litters_yr <- mean(mDat$litters_yr, na.rm = TRUE)
+
+#Mean value
+mean_litters_yr
+
+# Here i am Subtract the mean value from each individual value of `litters_yr`
+mDat$mean_centered_litters_yr <- mDat$litters_yr - mean_litters_yr
+
+#'*Why do you get an error message here?*
+#-----Reason---
+#'*We may encounter errors due to missing or negative values in the litters_yr variable*
+#'*Additionally, taking the logarithm of zero or negative values will result in NaN or Inf*
+#'* And We handle missing or negative values using na.rm = TRUE and by adding a small constant*
+
+mDat$log_mean_centered_litters_yr <- log(mDat$mean_centered_litters_yr + 1e-6)
+
+#Finally, we print the dataset to observe the results and potential error messages.
+mDat
+
+names(mDat) #This shows that our calculations have been added to the dataframe
+
+#For better obs, we export the dataframe into an excel
+write.xlsx(mDat, "Data/Dingani/final_datasets.xlsx", rowNames = FALSE)
 
 
+## Question 5:
+#1. Generate a vector of 1000 random numbers drawn from a normal distribution of mean = 100 and *variance* = 25.
+
+# Set the parameters
+mean_val <- 100
+variance_val <- 25
+
+# Calculate the standard deviation
+sd_val <- sqrt(variance_val)
+
+# Generating the vector of random numbers
+random_vector <- rnorm(1000, mean = mean_val, sd = sd_val)
+
+# output of the first few elements of the vector
+head(random_vector)
+
+random_vector
+
+#2. Now plot two histograms, side-by-side on the same single plot:
+
+#Here we use the df made in the previosu question
+
+random_vector
+
+# The we Create a vector containing the natural logarithm of the random numbers
+log_random_vector <- log(random_vector)
 
 
+# Because we want to use ggplot2, we Create a data frame with the random and log-random variables
+dat.random <- data.frame(random = random_vector, log_random = log_random_vector)
+
+
+# Plot the histograms using ggplot
+plt1 <- ggplot(dat.random, aes(x = random)) +
+  geom_histogram(binwidth = 5, fill = "skyblue") +
+  labs(title = "Histogram of Random Variable", x = "Value", y = "Frequency") +
+  theme_minimal()
+
+plt2 <- ggplot(dat.random, aes(x = log_random)) +
+  geom_histogram(binwidth = 0.05, fill = "lightgreen") +
+  labs(title = "Histogram of Log(Random Variable)", x = "Value", y = "Frequency") +
+  theme_minimal()
+
+plt1 + plt2 + plot_layout(guides = "collect")
